47d3ea6685bab6652a069056b77277d6d817e8be
John Lexter Reyes <johnlexterreyesss@gmail.com>
2025-03-05 13:40:12 +0800

    Deleted the test messages, added the processes of the system

--- Program.cs
+++ Program.cs
@@ -4,9 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello GitHub");
-            Console.WriteLine("Another test, hi Gaile");
+            string[] processes = {"1 - Add Laptop","2 - Display Laptop","3 - Recommend Laptop","4 - Remove Laptop","5 - Exit"};
+            Boolean loop = true;
+
+            while (loop)
+            {
+                Console.WriteLine("Welcome to Laptop Recommender!\n" +
+                    "Choose process");
+                foreach (string process in processes)
+                {
+                    Console.WriteLine(process);
+                }
+                int ProcessChoice = Convert.ToInt16(Console.ReadLine());
+
+                switch(ProcessChoice)
+                {
+                    case 1:
+                        Console.WriteLine("Adding laptop...");
+                        break;
+                    case 2:
+                        Console.WriteLine("Displaying laptop");
+                        break;
+                    case 3:
+                        Console.WriteLine("Recommeding laptop...");
+                        break;
+                    case 4:
+                        Console.WriteLine("Removing laptop");
+                        break;
+                    case 5:
+                        Console.WriteLine("Exiting...");
+                        Console.WriteLine("Thank you for using");
+                        loop = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
         }
     }
 }
