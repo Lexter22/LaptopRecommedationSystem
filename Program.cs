7aae42593e2da567d0788f1d3dac4226a8145745
John Lexter Reyes <johnlexterreyesss@gmail.com>
2025-03-05 13:22:35 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LaptopRecommedationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
