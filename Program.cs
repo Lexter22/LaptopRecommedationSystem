00bcb29e45129df6636ac7e40a21a915c9fafceb
John Lexter Reyes <johnlexterreyesss@gmail.com>
2025-03-05 13:32:13 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello GitHub");
+            Console.WriteLine("Another test, hi Gaile");
         }
     }
 }
